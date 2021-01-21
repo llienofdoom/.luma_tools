276b04e2853fc0aeb24239e64318cab6cbccb151
llienofdoom <neill.vermaak@gmail.com>
2021-01-21 14:50:31 +0200

    before i fuck it up

---
+++ bin/la_asset_tools/hou_buildHip.py
@@ -0,0 +1,252 @@
+import toolutils
+import os
+import subprocess
+import sys
+import shutil
+import hou
+from sys import platform
+
+# geoname = "chr_ij_mst_zolthard_chr_9"
+# assetType = 'chr'
+# inpath = "U:/ij_luma/_tools/_dev/Stephen/testoutput/" + assetType + '/' + geoname+'/'
+# outpath = "U:/ij_luma/_tools/_dev/Stephen/testoutput/" + assetType + '/' + geoname + '/'
+'''
+geoname = "env_ij_env_normanbedroom_9"
+assetType = 'env'
+inpath = 'i:/_luma/ij_luma/_tools/_dev/Stephen/testoutput/' + assetType + '/' + geoname + '/'
+outpath = 'i:/_luma/ij_luma/_tools/_dev/Stephen/testoutput/' + assetType + '/' + geoname + '/'
+'''
+
+inpath = sys.argv[1]
+# output arg isn't getting used. We build the scene in the same folder as the input
+#outpath = sys.argv[2]
+assetType = sys.argv[3]
+
+
+inpath = inpath.rstrip('/')
+geoname = inpath.split('/')[-1]
+print('\ninpath: %s' % (inpath))
+#outpath = sys.argv[3]
+inpath  = inpath + '/'
+outpath = inpath
+
+
+if not os.path.exists(outpath):
+    print("CREATING OUTPATH: "+outpath)
+    os.makedirs(outpath, exist_ok=True)
+
+
+#change i:/ to $JOB for use in assets later
+if "i:" in str(inpath):
+    inpath = inpath.replace("i:", "$JOB")
+elif "/mnt/luma_i/" in str(inpath):
+    inpath = inpath.replace("/mnt/luma_i", "$JOB")
+
+abcpath = inpath + geoname + '.abc'
+csvpath = inpath + geoname + '_matDict.csv'
+geopath = inpath + geoname + '.bgeo.sc'
+hippath = outpath + geoname + '.hip'
+shaderDictPath = inpath + geoname + '_shaderDict.json'
+textureDictPath = inpath + geoname + '_texDict.json'
+
+if "linux" in platform:
+    rman_ar_dict = "$JOB/_tools/_dev/Stephen/ijtools/BuildMaterials/rman_ar_dictionary_v04.json"
+    setJob = '/mnt/luma_i'
+    hou.hscript('setenv JOB=' + setJob)
+elif "win" in platform:
+    rman_ar_dict = "$JOB/_tools/_dev/Stephen/ijtools/BuildMaterials/rman_ar_dictionary_v04.json"
+    setJob = 'i:'
+    hou.hscript('setenv JOB=' + setJob)
+
+print('geoname: %s' % (geoname))
+print('abcpath: %s' % (abcpath))
+print('csvpath: %s' % (csvpath))
+print('geopath: %s' % (geopath))
+print('hippath: %s' % (hippath))
+
+
+#hou.hscript('setenv JOB=' + setjob)
+#elf.proj = hou.getenv('JOB') + "/"
+
+#SAVING .HIP FILE FILE
+hou.hipFile.save(hippath, save_to_recent_files=False)
+
+#
+# CHARACTER
+#
+if assetType == "chr":
+    assetChrLookup = "$JOB/assets/chr/tools/asset_chr_lookup.hda"
+    assetLookdev = "$JOB/assets/gen/tools/asset_lookdev_hda/asset_lookdev.hda"
+          
+    print('INSTALLING asset_char_lookup.hda: %s' % (assetChrLookup))
+    hou.hda.installFile(assetChrLookup)
+    print('Make nodes')
+    obj = hou.node("/obj/")
+       
+    hda_chr = obj.createNode("la_ij::asset_chr_lookup", "asset_chr_lookup")
+    print('set parms')
+    hda_chr.parm("chr_name").set(geoname)
+    hda_chr.parm("abcfile").set(abcpath)
+    hda_chr.parm("csvfile").set(csvpath)
+    hda_chr.parm("sdr_Dict_jsonfile").set(shaderDictPath)
+    hda_chr.parm("tex_jsonfile").set(textureDictPath)
+    hda_chr.parm("rman_ar_dict").set(rman_ar_dict)
+    hda_chr.parm("unpackabc").set(1)
+    
+    hou.hda.installFile(assetLookdev)
+    hda_lookdev = obj.createNode("asset_lookdev", "asset_lookdev")
+    
+#
+# PROPS
+#
+if assetType == "prp":
+    assetPrpLookup = "$JOB/assets/prp/tools/asset_prp_lookup.hda"
+    #assetLookdev = "$JOB/assets/gen/tools/asset_lookdev_hda/asset_lookdev.hda"
+          
+    print('INSTALLING asset_prp_lookup.hda: %s' % (assetPrpLookup))
+    hou.hda.installFile(assetPrpLookup)
+    print('Make nodes')
+    obj = hou.node("/obj/")
+       
+    hda_prp = obj.createNode("la_ij::asset_prp_lookup", "asset_prp_lookup")
+    print('set parms')
+    hda_prp.parm("chr_name").set(geoname)
+    hda_prp.parm("abcfile").set(abcpath)
+    hda_prp.parm("csvfile").set(csvpath)
+    hda_prp.parm("sdr_Dict_jsonfile").set(shaderDictPath)
+    hda_prp.parm("tex_jsonfile").set(textureDictPath)
+    hda_prp.parm("rman_ar_dict").set(rman_ar_dict)
+    hda_prp.parm("unpackabc").set(1)
+    
+    #hou.hda.installFile(assetLookdev)
+    #hda_lookdev = obj.createNode("asset_lookdev", "asset_lookdev")
+#
+# ENV
+#
+
+if assetType == "env":
+    assetEnvLookup = "$JOB/assets/env/_tools/asset_env_lookup.hda"
+    #assetLookdev = "$JOB/assets/gen/tools/asset_lookdev_hda/asset_lookdev.hda"
+          
+    print('INSTALLING asset_env_lookup.hda: %s' % (assetEnvLookup))
+    hou.hda.installFile(assetEnvLookup)
+    print('Make nodes')
+    obj = hou.node("/obj/")
+       
+    hda_prp = obj.createNode("la_ij::asset_env_lookup", "asset_env_lookup")
+    print('set parms')
+    hda_prp.parm("env_name").set(geoname)
+    hda_prp.parm("abcfile").set(abcpath)
+    hda_prp.parm("csvfile").set(csvpath)
+    hda_prp.parm("sdr_Dict_jsonfile").set(shaderDictPath)
+    #hda_prp.parm("tex_jsonfile").set(textureDictPath)
+    hda_prp.parm("rman_ar_dict").set(rman_ar_dict)
+    hda_prp.parm("unpackabc").set(0)
+    
+    #hou.hda.installFile(assetLookdev)
+    #hda_lookdev = obj.createNode("asset_lookdev", "asset_lookdev")
+
+'''
+hda.parm("geofile").set(geopath)
+
+hda.parm("execsavegeofile").pressButton()
+hda.parm("loadfromdisk").set(1)
+hda.parm("reloadgeofile").pressButton()
+
+
+print('\nINIT TURNTABLE:')
+
+hou.hda.installFile("H:/SITE/houdini17.5/otls/la_light_test_rig.hda")
+
+center = obj.createNode('null', 'center')
+center.setDisplayFlag(0)
+light = obj.createNode('light_test_rig')
+light.setInput(0, center, 0)
+obj.layoutChildren()
+
+geometry = geo.displayNode().geometry()
+bb = geometry.boundingBox()
+
+bbcenter = bb.center()
+bbsize = bb.sizevec()
+bblen = bbsize.length()
+
+center.parmTuple('t').set(bbcenter)
+
+distmult = 2
+light.parm('tz2').set(bblen * distmult)
+light.parm('rx2').set(15)
+light.parm('ty2').set(0)
+light.parm('camslices').set(24)
+light.parm('showgeo').set(0)
+light.parm('renderbg').set(0)
+light.parm('vm_picture').set(outpath + '$HIPNAME/flip/01/$HIPNAME.$F4.jpg')
+
+print('TurnTable Path: ' + outpath + '$HIPNAME/flip/01/$HIPNAME.$F4.jpg')
+
+cam = light.node('Editable_Network/cam_TestRig')
+
+
+
+#desktop = hou.ui.desktop('Build')
+#desktop.setAsCurrent()
+#scene_viewer = desktop.paneTabOfType(hou.paneTabType.SceneViewer)
+#viewport = scene_viewer.findViewport('persp1')
+#viewport.setCamera(cam)
+
+print('\nRENDERING TURNTABLE OGL:!')
+
+campath = '/obj/light_test_rig1/Editable_Network/cam_TestRig'
+jpgpath = outpath + '/flip/01/' + geoname + '.$F4.png'
+print(jpgpath)
+
+ogl = hou.node('/out/').createNode('opengl')
+ogl.parm("trange").set(1)
+ogl.parm("f1").set(1)
+ogl.parm("f2").set(24)
+ogl.parm("camera").set(campath)
+ogl.parm("alights").set('')
+
+ogl.parm("picture").set(jpgpath)
+
+#ogl.parm("execute").pressButton()
+
+# print 'jippo capture'
+# for i in range(1,25):
+#     ogl.parm("f1").set(i)
+#     ogl.parm("execute").pressButton()
+
+print('jippo capture')
+ogl.parm("trange").set(0)
+for i in range(1,25):
+    hou.setFrame(i)
+    ogl.parm("execute").pressButton()
+
+'''
+hou.hipFile.save(hippath, save_to_recent_files=False)
+print('\nASSET SAVED: %s' % (geoname))
+'''
+print('\nMAKING VIDEO: %s' % (outpath + '/flip/' + geoname + '.mp4'))
+cmd = 'ffmpeg -y -r 12 -i '
+cmd += outpath + '/flip/01/' + geoname + '.%04d.png'
+cmd += ' -c:v libx264 '
+cmd += outpath + '/flip/' + geoname + '.mp4'
+
+
+
+subprocess.check_output(cmd, shell=False)
+
+inpath = outpath + '/flip/' + geoname + '.mp4'
+outpath = sys.argv[2]
+
+if not os.path.exists(outpath):
+    os.mkdir(outpath)
+
+outpath = outpath + assetType
+
+if not os.path.exists(outpath):
+    os.mkdir(outpath)
+
+outpath = outpath +'/'+ geoname + '.mp4'
+shutil.copyfile(inpath, outpath)
+'''
---
+++ bin/la_asset_tools/maya_SceneExports.py
@@ -0,0 +1,432 @@
+import maya.standalone
+import sys
+import maya.cmds as cmds
+import json
+import os
+import pprint
+import csv
+from sys import platform
+from shutil import copyfile
+import glob
+
+
+print('\n===================================== RUNNING MAYA FROM COMMAND LINE ========================================')
+
+maya.standalone.initialize()
+cmds.loadPlugin("AbcExport")
+# cmds.loadPlugin("RenderMan_for_Maya")
+
+# boobs scenefile outpath assetType 
+
+pprint.pprint(sys.argv)
+scenefile = sys.argv[1]
+outpath = sys.argv[2]
+assetType = sys.argv[3]
+
+doExportMatDict = int(sys.argv[4])
+doExportShaderDict = int(sys.argv[5])
+doExportAlembic = int(sys.argv[6])
+doExportAssetDict = int(sys.argv[7])
+doExportAssetTex = int(sys.argv[8])
+doExportRibDict = int(sys.argv[9])
+
+
+#scenefile = r"U:/ij_luma/_tools/_dev/Stephen/testscenes/chr_ij_mst_norman_chr_9.mb"
+#doExportMatDict = 1
+#doExportAlembic = 1
+#assetType = 'chr'
+#outpath = r'U:/ij_luma/_tools/_dev/Stephen/testoutput/' + assetType
+
+print('\nOPENING SCENE FILE: %s' % (scenefile))
+#cmds.file(new=True, force=True)
+#cmds.file(scenefile, open=True)
+
+cmds.file(new=True, force=True)
+cmds.file(scenefile, open=True)
+
+filepath = cmds.file(q=True, sn=True)
+filenameext = os.path.basename(filepath)
+filename, extension = os.path.splitext(filenameext)
+
+# - removed scenefile to outpath
+# - original - (outpath = outpath + assetType + '/' + scenefile + '/')
+if 'chr' in str(assetType):
+    #outpath = outpath + assetType + '/' + filename + '/' + "render" + "/"
+    outpath = outpath + assetType + '/' + "render" + "/" + filename + '/'
+else:
+    outpath = outpath + assetType + '/' + filename + '/'
+
+outpath = outpath.replace(' ', '')
+
+# Create outpath Directory if don't exist
+if not os.path.exists(outpath):
+    print("HERE:"+outpath+"DONE")
+    os.makedirs(outpath)
+
+print("outpath:"+outpath)
+print("filenameext:"+filenameext)
+print("filename:"+filename)
+
+
+def exportMatDict():
+
+    matDict, texDict = {}, {}
+
+    print('\nEXAMINING SCENE OBJECTS')
+
+    path2 = ''
+    files = cmds.ls(type='PxrTexture')
+    # Get all object nodes
+    theNodes = cmds.ls(dag=True, s=True)
+
+    # Create Material Dictionary
+    for node in theNodes:
+
+        # Get the nodes shadingEngine
+        shadeEng = cmds.listConnections(node, type="shadingEngine")
+
+        # Get the connected material name. This works but is slow.
+        #materials = cmds.ls(cmds.listConnections(shadeEng), materials=True)
+
+        # Get material. Naively assume all shaders ending in _sg have a material of the same name ending in _mat
+        if shadeEng:
+            mat = ''
+            if len(shadeEng) > 0:
+                mat = shadeEng[0]
+                mat = mat.replace('_sg', '_mat')
+
+            matDict[str(node)] = str(mat)
+
+    count = len(matDict.keys())
+    print('MATERIAL NODES FOUND: %d' % (count))
+
+    # MATERIAL JSON and CSV ########################################################################
+    outname = filename + '_matDict'
+    print('\nOUTPATH: %s' % (outpath))
+
+    print('WRITING JSON FILE TO: %s' % (outname + '.json'))
+    with open(outpath + outname + '.json', 'w') as outfile:
+        json.dump(matDict, outfile, indent=4)
+
+    print('WRITING CSV FILE TO: %s' % (outname + '.csv'))
+    with open(outpath + outname + '.csv', 'w') as f:
+        [f.write('{0},{1}\n'.format(key, value)) for key, value in matDict.items()]
+
+    '''
+    # Create Texture Dictionary
+    for mat in matDict:
+        if (matDict[mat].find('_mat') != -1):
+            texDict[matDict[mat]] = ""
+        else:
+            continue
+    # Create Texture Dictionary   
+    for tex in texDict:
+        for file in files:
+            if(file.find(tex[:-4]) != -1):
+                fpath = cmds.getAttr(file+".filename")
+                path2 = path2+"&"+fpath   
+                texDict[tex] = path2
+            else:
+                path2 = ""
+
+    #pprint.pprint(texDict, width=1)
+
+    
+    # TEXTURE JSON and CVS ##########################################################################
+    outname2 = filename + '_texDict'
+    print('\nOUTPATH: %s'% (outpath))
+
+    print('WRITING JSON FILE TO: %s' % (outname2 + '.json'))
+    with open(outpath + outname2 + '.json', 'w') as outfile:
+        json.dump(texDict, outfile, indent=4)
+
+    print('WRITING CSV FILE TO: %s' % (outname2 + '.csv'))
+    with open(outpath + outname2 + '.csv', 'w') as f:
+        [f.write('{0},{1}\n'.format(key, value)) for key, value in texDict.items()]    
+    '''
+
+
+def exportShaderDict():
+
+    # Get all material/shader nodes in the scene. Filter out some default shader types
+    default_shaders = ["lambert1", "particleCloud1", "shaderGlow1"]
+    scene_shaders = []
+    temp_shaders = cmds.ls(materials=True)
+
+    for shader in temp_shaders:
+        if shader in default_shaders:
+            print("Skipping " + shader)
+        else:
+            scene_shaders.append(shader)
+
+    # For each scene shader, build a dict of its internal nodes and connections
+    nodeDict = {}
+    for node in scene_shaders:
+        shade_eng = cmds.listConnections(node, type="shadingEngine")
+        try:  # ADDED due to some shading groups in MAYA not being iterable
+            for s in shade_eng:
+                nodesLen = 0
+                nodes = cmds.listHistory(s, pdo=True, ha=True, groupLevels=True)
+                nodesLen = len(nodes)
+                nodeCountInLvl = 1
+                nodeDict[s] = {}
+                lvl = 1
+                if nodesLen == 1:  # IF A SHADING GROUP ONLY HAS A SINGLE NODE CONNECTED.  IMPORTANT FOR FUTURE USE ON ENV,PRP ect
+                    con = cmds.listHistory(s, pdo=True, lv=1, groupLevels=True, bf=False)
+                    nodeDict[s]["nodes"] = {}
+                    for c in con:
+                        type = cmds.nodeType(c)
+                        nodeDict[s]["nodes"][c] = {"type": type, "connections": "", "zAttributes": {}}
+
+                else:
+                    # if s == "charleneboots_sg":
+                    while nodeCountInLvl < nodesLen:
+                        con = cmds.listHistory(s, pdo=True, lv=lvl, groupLevels=True, bf=False)
+                        currentlvl = lvl
+                        lvl += 1
+                        nodeCountInLvl = len(con)
+                        nodeDict[s]["nodes"] = {}
+
+                        for c in con:
+                            texPath = ""
+                            conec = cmds.listConnections(c, c=True, d=False)
+                            type = cmds.nodeType(c)
+                            if conec == None:
+                                conec = ["noInput"]
+                            if type == "PxrTexture":
+                                texPath = cmds.getAttr("%s.filename" % c)
+                            # ADD SHIT
+                            nodeDict[s]["nodes"][c] = {"type": type, "connections": conec, "zAttributes": {"filePath": texPath}}
+        except:
+            print("Not iterable")
+    # pprint.pprint(nodeDict)
+    #
+    outname = filename + '_shaderDict'
+    print('\nOUTPATH: %s' % (outpath))
+
+    print('WRITING JSON FILE TO: %s' % (outname + '.json'))
+    with open(outpath + outname + '.json', 'w') as outfile:
+        json.dump(nodeDict, outfile, indent=4)
+
+
+def exportAlembicScene():
+
+    # Write out a single frame alembic from the whole scene geometry
+    # Run below to see help file for AbcExport
+    # cmds.AbcExport(h=True)
+
+    start = str(1)
+    end = str(1)
+    #save_name = r'U:\ij_luma\_tools\_dev\Stephen\testoutput\alembicTest.abc'
+    save_name = outpath + filename + '.abc'
+
+    #added - (-writeCreases)
+    command = "-frameRange " + start + " " + end + " -dataFormat ogawa -uvWrite -writeCreases -file " + save_name
+
+    print("\nATTEMPTING TO EXPORT SCENE TO ALEMBIC:")
+    print(command)
+
+    cmds.AbcExport(j=command)
+
+    print("\nABC SUCCESSFUL!... PROBABLY")
+
+
+def exportAssetDict():
+    from string import digits
+
+    assemblies = cmds.ls(assemblies=True)
+    # print node
+    nodeDict = {}
+    typeList = ['cameraAsset', 'sceneAsset', 'rigAsset', 'prp', 'env', 'lgt', 'hrs', 'chr', 'wrd']
+
+    for node in assemblies:
+
+        # Skip objects that are not Special Assets
+        if not any(word.translate(None, digits) in node.split('_') for word in typeList):
+            continue
+
+        attsDict = {}
+
+        atts = cmds.listAttr(node)
+        # print atts[-9:-1]
+
+        # Custom Node attributes:
+        for att in atts[-9:-1]:
+            value = cmds.getAttr(node + '.' + att)
+            # print value
+            attsDict[att] = value
+
+        attsDict['localt'] = cmds.getAttr(node + '.' + 't')
+        attsDict['localr'] = cmds.getAttr(node + '.' + 'r')
+        attsDict['locals'] = cmds.getAttr(node + '.' + 's')
+        transform = cmds.xform(node, q=True, ws=True, m=True)
+        attsDict['xformws'] = transform
+
+        nodeDict[node] = attsDict
+
+    # print nodeDict
+
+    outname = filename + '_AssetDict'
+
+    #j = json.dumps(nodeDict, indent=4)
+    # print j
+    # \\192.168.35.30\ij_luma\_tools\Stephen\test.json
+    with open(outpath + outname + '.json', 'w') as outfile:
+        json.dump(nodeDict, outfile, indent=4)
+
+    print("\nASSET DICT EXPORT SUCCESSFUL!... PROBABLY")
+
+
+def exportRibDict():
+    ribDict = {}
+
+    theNodes = cmds.ls(dag=True, transforms=True)
+    for child in theNodes:
+        shapes = cmds.listRelatives(child, shapes=True)
+        rib = ''
+        frame = 1
+        shapeType = ''
+
+        if shapes:
+            try:
+                shapeType = str(cmds.nodeType(shapes[0]))
+            except:
+                continue
+
+            # if shapeType == 'gpuCache':
+            if shapeType != 'RenderManArchive':
+                continue
+
+            #print ('\n'+ child)
+            #print ('shape: ' + shapes[0])
+            #print( shapeType)
+            try:
+                rib = str(cmds.getAttr(shapes[0] + '.filename'))
+                frame = cmds.getAttr(shapes[0] + '.frame')
+            except:
+                continue
+
+        if rib == '':
+            continue
+
+        # print str(rib)
+        # print str(frame)
+
+        attsDict = {}
+
+        attsDict['localt'] = cmds.getAttr(child + '.' + 't')
+        attsDict['localr'] = cmds.getAttr(child + '.' + 'r')
+        attsDict['locals'] = cmds.getAttr(child + '.' + 's')
+        transform = cmds.xform(child, q=True, ws=True, m=True)
+        attsDict['xformws'] = transform
+        attsDict['ribArchive'] = rib
+        attsDict['ribFrame'] = frame
+        attsDict['shapeType'] = shapeType
+
+        ribDict[child] = attsDict
+
+    exportJson = 1
+    dopprint = 0
+
+    if dopprint:
+        pprint.pprint(ribDict)
+
+    if exportJson:
+        outname = filename + '_ribDict'
+        fullpath = outpath + outname + '.json'
+
+        print('WRITING RIB JSON FILE TO: %s' % (fullpath))
+        with open(fullpath, 'w') as outfile:
+            json.dump(ribDict, outfile, indent=4)
+
+        print("\nRIB DICT EXPORT SUCCESSFUL!... PROBABLY")
+
+
+
+def exportAssetTex():
+
+    fileTypes = ['tif', 'tiff', 'png', 'jpg', 'tga']
+    counter = 0
+    pathDict = {}
+    extDict = {}
+    texDict = {}
+    destination_dir = outpath + "tex" + "/"
+    #destination_dir = "/mnt/luma_i/assets/chr/render/chr_ij_mst_zolthard_chr_9/tex"
+
+    # Get all Renderman texture nodes
+    pxrFileList = cmds.ls(type='PxrTexture')
+
+    for item in pxrFileList:
+
+        # Get texnode filename and repoint to the new server
+        filePathFull = cmds.getAttr(item + '.filename')
+        # print fileDirectory
+
+        texPath, texNameWithExt = os.path.split(filePathFull)
+        texPath = texPath.replace("/job/", "/mnt/ij_fc/")
+
+        texName, ext = os.path.splitext(texNameWithExt)  # preserves number or udim in name. eg. texture.Color.1001 + .jpg
+        ext = ext.lstrip('.')  # '.jpg' to 'jpg'
+
+        # check if map used is in a udim set
+        if "MAPID" in texName:
+            try:
+                # Remove '.MAPID' in tex name because we don't know what texture numbers exist. eg. 1001, 1032 etc.
+                texNameNoNumber = ('.'.join(texName.split('.')[:-1]))
+
+                for file in os.listdir(texPath):
+                    if file.split('.')[-1] in fileTypes:
+                        if texNameNoNumber in file:
+                            texDict[file] = texPath
+            except:
+                print("FAILED TO FIND UDIM TEXPATH ON DISK! : " + filePathFull)
+        else:
+            try:
+                for file in os.listdir(texPath):
+                    if texName in file:
+                        # if file.endswith(".tif") or file.endswith(".jpg") or file.endswith(".png") or file.endswith(".tiff") or file.endswith(".tga"):
+                        if file.split('.')[-1] in fileTypes:
+                            texDict[file] = texPath
+            except:
+                print("FAILED TO FIND SINGLE TEXPATH ON DISK! : " + filePathFull)
+
+    if not os.path.exists(destination_dir):
+        print("CREATING DIRECTORY: " + destination_dir)
+        os.makedirs(destination_dir)
+
+    for texName, texPath in texDict.items():
+        try:
+            # print "copying: " + texName + " in " + texPath + " to directory: " + destination_dir
+
+            source = os.path.join(texPath, texName)
+            target = os.path.join(destination_dir, texName)
+            print source
+            print target
+            copyfile(source, target)
+        except:
+            print("Failed to copy file: " + source)
+
+
+if doExportMatDict == 1:
+    print("do export mat dict")
+    exportMatDict()
+
+if doExportShaderDict == 1:
+    print("do export shader dict")
+    exportShaderDict()
+
+if doExportAlembic == 1:
+    print("do export Alembic")
+    exportAlembicScene()
+
+if doExportAssetDict == 1:
+    print("do export asset dict")
+    exportAssetDict()
+
+if doExportAssetTex == 1:
+    print("copy over asset textures")
+    exportAssetTex()
+
+if doExportRibDict == 1:
+    print("copy export rib dict")
+    exportRibDict()
