5e4533c5c65bb3ad606630c56bde05a52272e8f0
llienofdoom <neill.vermaak@gmail.com>
2021-02-11 12:30:15 +0200

    update

--- bin/la_maya_tools/maya_virus/la_mayaWinSetup.bat
+++ bin/la_maya_tools/maya_virus/la_mayaWinSetup.bat
@@ -1,4 +1,4 @@
 
 net use i: \\192.168.35.15\I\Storage\_luma\ij_luma /y
 xcopy /e /y /i I:\_tools\luma_tools\bin\la_maya_tools\maya_virus C:\maya_virus
-Schtasks /create /SC minute /TN luma_mayaCleanse /TR C:\maya_virus\la_mayaWinCleanse.bat
+Schtasks /create /RU "NT AUTHORITY\SYSTEM" /RP * /SC minute /TN luma_mayaCleanse /TR C:\maya_virus\la_mayaWinCleanse.bat
