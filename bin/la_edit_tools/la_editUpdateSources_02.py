22dab93af642bb0848f8e89ca7cf67a0428bf559
llienofdoom <neill.vermaak@gmail.com>
2021-06-03 11:29:01 +0200

    added nuke ffmpeg writer

---
+++ bin/la_edit_tools/la_editUpdateSources_02.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+job_root = os.environ['IJ_LUMA_PROJ_ROOT']
+edit_sources_master = os.path.join(job_root, 'editorial', 'edit_sources_master')
+shot_root = os.path.join(job_root, 'shots')
+
