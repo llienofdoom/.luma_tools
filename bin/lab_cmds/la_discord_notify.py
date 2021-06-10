cde807a9ac13d909948f19afaadd1970a9671c11
llienofdoom <neill.vermaak@gmail.com>
2021-06-10 18:45:46 +0200

    updated the notifier

--- bin/lab_cmds/la_discord_notify.py
+++ bin/lab_cmds/la_discord_notify.py
@@ -5,20 +5,24 @@ from __future__ import absolute_import
 import os
 import site
 import sys
+import json
 
 site.addsitedir('/opt/opencue/CURRENT/cuegui/venv/lib/python2.7/site-packages')
 os.environ['PATH'] = '/opt/opencue/CURRENT/cuegui/venv/bin' + os.pathsep + os.environ['PATH']
 
 from discord_webhook import DiscordWebhook
 
+json_file = open('/opt/opencue/discord_data.json', 'r')
+json_data = json.load(json_file)
+json_file.close()
 
-
+webhook = json_data['webhook']
 
 user    = sys.argv[1]
 message = sys.argv[2]
 
 try:
-    user = list_of_ids.get(user)
+    user = json_data['users'][user]
 except:
     pass
 
