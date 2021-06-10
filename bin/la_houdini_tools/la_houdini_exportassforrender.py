b352cf2f22fa69d134f647b9614835c25a3deed8
llienofdoom <neill.vermaak@gmail.com>
2021-06-10 15:51:35 +0200

    starting hip exporter

---
+++ bin/la_houdini_tools/la_houdini_exportassforrender.py
@@ -0,0 +1,41 @@
+#####################################################################
+# H:\_distros\hfs.windows-x86_64_16.5.473\bin\hython.exe
+# H:\_distros\_lumatools\lumatools\luma_houdiniHythonRender.py
+# -h X:\_studiotools\TMP\HQ\mantra_test\mantra_test_103.hip
+# -r "/out/rop_hm_base"
+# -fs 1
+# -fe 10
+#####################################################################
+
+import sys
+import os
+import hou
+
+print sys.argv
+
+"""
+hipfile = sys.argv
+
+print 'Exporting ASS files to render, from', hipfile
+
+print "Loading file %s" % (hipfile)
+hou.hipFile.load(hipfile)
+print "Loaded!"
+
+hou.parm('/obj/ij_stereo_camera_rig/ar_skip_license_check').set(0)
+
+fs  = int(sys.argv[6])
+fe  = int(sys.argv[8])
+print "Setting frame range to %d - %d" % (fs, fe)
+
+# Set progress output
+rop.parm('vm_verbose').set(3)
+rop.parm('vm_alfprogress').set(1)
+
+# RENDER
+print "Starting to render...",
+rop.render(frame_range=(fs, fe))
+print "DONE!"
+
+quit()
+"""
