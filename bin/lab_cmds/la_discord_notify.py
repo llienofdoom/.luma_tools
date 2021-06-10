a633d54043a0b58bea9204179e04909ba61ff063
llienofdoom <neill.vermaak@gmail.com>
2021-06-10 18:31:43 +0200

    removed discord data

--- bin/lab_cmds/la_discord_notify.py
+++ bin/lab_cmds/la_discord_notify.py
@@ -11,12 +11,8 @@ os.environ['PATH'] = '/opt/opencue/CURRENT/cuegui/venv/bin' + os.pathsep + os.en
 
 from discord_webhook import DiscordWebhook
 
-webhook = 'https://discord.com/api/webhooks/852533533020389406/mjLWCudOguVlLh0oDHuY1xyFGwuzQnHHPArIBkBHR7ZrXCWMNPxlyJVsmvAloGZlaVAU'
 
-list_of_ids = {
-    'neill' : '<@!278447205722095616>',
-    'christophe' : '<@!278442049651474432>',
-}
+
 
 user    = sys.argv[1]
 message = sys.argv[2]
