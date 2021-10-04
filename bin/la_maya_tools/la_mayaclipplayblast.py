74f2dadf98d15d80a9315a68dad4ecac9a729520
llienofdoom <neill.vermaak@gmail.com>
2021-10-04 15:35:35 +0200

    updates

--- bin/la_maya_tools/la_mayaclipplayblast.py
+++ bin/la_maya_tools/la_mayaclipplayblast.py
@@ -61,7 +61,7 @@ if renderCamera != None:
     cmd += ' -start_number %d' % frame_s
     cmd += ' -i %s' % playblast_path + '.%04d.png'
     cmd += ' -s 2048x1152'
-    cmd += ' -filter_complex "[0:v]drawtext=\'fontcolor=white:font=sans-serif:fontsize=12:x=6:y=600:text=  luma-film - 2020 - inside job %s - %s - %s - %%{frame_num}:box=1:boxborderw=5:boxcolor=black:start_number=%d\'[LT]"' % (date, shot_name, user, frame_s)
+    cmd += ' -filter_complex "[0:v]drawtext=\'fontcolor=white:font=sans-serif:fontsize=12:x=6:y=550:text=  luma-film - 2020 - inside job %s - %s - %s - %%{frame_num}:box=1:boxborderw=5:boxcolor=black:start_number=%d\'[LT]"' % (date, shot_name, user, frame_s)
     cmd += ' -pix_fmt yuv420p -c:v libx264 -crf 25 -map "[LT]"'
     cmd += ' %s' % video_path
     os.system( cmd )
