88b8e7258f4ff1930d5670d39718a78703a669ca
llienofdoom <neill.vermaak@gmail.com>
2021-10-04 11:47:24 +0200

    added render scene view

---
+++ bin/lab_cmds/la_mrviewer_scene_render.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import glob
+
+if len(sys.argv) == 2:
+    scene = 'sc%03d' % int(sys.argv[1])
+    print 'Viewing Mono Scene', scene
+    scene_folder = glob.glob('/mnt/luma_i/shots/*/' + scene)
+    if len(scene_folder) > 0:
+        command = 'mrViewer --edl '
+        scene_folder = scene_folder[0]
+        for (dirpath, dirnames, filenames) in os.walk(scene_folder):
+            for shot in dirnames:
+                shot_path = scene_folder + os.sep + shot + os.sep + 'img/renders/act*_sc*_sh*_render_v*.mp4'
+                vid = glob.glob(shot_path)
+                if len(vid) > 0:
+                    vid = vid[-1]
+                else:
+                    continue
+                command += vid + ' '
+            break
+        os.system(command)
+
+else:
+    print "Please specify a scene you want to watch."
+    exit()
