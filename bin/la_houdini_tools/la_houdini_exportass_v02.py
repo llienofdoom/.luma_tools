61ef53e7ff737356b26874f3848b4806bb57eb1b
shotalot <shotalot@gmail.com>
2021-12-04 18:22:00 +0200

    Updates to ass file script: - script now updates camera rig to latest version if final render flag on - fix double caching issue making caching take longer... - start adding final render parameter

--- bin/la_houdini_tools/la_houdini_exportass_v02.py
+++ bin/la_houdini_tools/la_houdini_exportass_v02.py
@@ -1,12 +1,3 @@
-#####################################################################
-# H:\_distros\hfs.windows-x86_64_16.5.473\bin\hython.exe
-# H:\_distros\_lumatools\lumatools\luma_houdiniHythonRender.py
-# -h X:\_studiotools\TMP\HQ\mantra_test\mantra_test_103.hip
-# -r "/out/rop_hm_base"
-# -fs 1
-# -fe 10
-#####################################################################
-
 import sys
 import os
 import time
@@ -17,6 +8,7 @@ hipfile = sys.argv[1]
 frame = int(sys.argv[2])
 mode = int(sys.argv[3])
 res = int(sys.argv[4])
+final = int(sys.argv[5])
 
 print 'Exporting ASS files to render, from %s.' % hipfile
 print 'Doing frame %04d.' % frame
@@ -28,7 +20,11 @@ print "Loaded!"
 time_end = time.time() - time_start
 print 'Scene loading took %d seconds.' % (time_end + 1)
 
-print 'Setting settings...'
+print 'Setting global settings...'
+
+print 'Render Mode is %d' % (mode)
+
+#GLOABAL SETTINGS
 hou.parm('/obj/ij_stereo_camera_rig/ar_skip_license_check').set(0)
 hou.parm('/obj/ij_stereo_camera_rig/enable_volumes').set(0)
 hou.parmTuple('/obj/ij_stereo_camera_rig/f').deleteAllKeyframes()
@@ -38,31 +34,42 @@ hou.parm('/obj/ij_stereo_camera_rig/view_cache').set(1)
 hou.parm('/obj/ij_stereo_camera_rig/rendertype').set(mode)
 hou.parm('/obj/ij_stereo_camera_rig/main_res').set(res)
 
+#FORCE FINAL RENDER PARAMETERS
+if final == 1:
+    print 'Rendering with final parameters forced on...'
+
+    #Update to latest render hda ver
+    print 'Updating to latest render hda...'
+    rendernode = hou.node("/obj/ij_stereo_camera_rig/")
+    library_filepath = rendernode.type().definition().libraryFilePath()
+    all_definitions = hou.hda.definitionsInFile(library_filepath)
+    nodename = all_definitions[-1].nodeTypeName()
+    nodetype = rendernode.type()
+    if nodetype != nodename:
+        rendernode.changeNodeType(all_definitions[-1].nodeTypeName())
+        rendernode = hou.node("/obj/ij_stereo_camera_rig/")
+
+    #Lock hda
+    rendernode.matchCurrentDefinition()
+
+    #Final render settings (WIP)
+    # TODO: MAKE SURE SIMS ARE ON FOR MAIN CHARACTERS
+    print 'Setting final render settings...'
+    hou.parm('/obj/ij_stereo_camera_rig/enable_secondary').set(0)
+    hou.parm('/obj/ij_stereo_camera_rig/main_res').set(0)
+
 # RENDER
 time_start = time.time()
-print "Starting to render... ############################## ",
-print "                      ############################## ",
-print "# ",
-print "# ",
-print "# ",
-print "# ",
-print "Caching Scene............. ##############################",
-hou.parm(
-    '/obj/ij_stereo_camera_rig/ropnet_RENDER/USECACHE/execute').pressButton()
-print "                           ##############################",
-print "# ",
-print "# ",
-print "Writing Passes............. ##############################",
+print "Caching Scene..."
 hou.parm('/obj/ij_stereo_camera_rig/ropnet_RENDER/brain_extra_passes/execute'
          ).pressButton()
 
 time_end = time.time() - time_start
 
-print "Done",
-print "# ",
-print "# ",
-
+print "Done."
+print ""
+print ""
 print 'Ass file exports took %d seconds.' % (time_end + 1)
-print "# ",
-print 'Exports complete. Exiting..................'
+print " "
+print 'Exports complete.'
 quit()
