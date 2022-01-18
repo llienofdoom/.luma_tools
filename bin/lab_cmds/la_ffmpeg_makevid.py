af678ee2542b266672cce6d08e7adc46d7f932c9
llienofdoom <neill.vermaak@gmail.com>
2022-01-18 23:06:52 +0200

    updates

--- bin/lab_cmds/la_ffmpeg_makevid.py
+++ bin/lab_cmds/la_ffmpeg_makevid.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 date = os.environ['IJ_DATE']
-shot_name = os.environ['IJ_SHOT_NAME']
+# shot_name = os.environ['IJ_SHOT_NAME']
 user = os.environ['IJ_USER']
 
 if len(sys.argv) == 2:
@@ -15,6 +15,7 @@ if len(sys.argv) == 2:
     list_of_files = glob.glob(search_pattern + '.*.' + search_extention)
     list_of_files.sort()
     first_frame = int(list_of_files[0].split('.')[1])
+    shot_name = "%s_%s_%s" % (list_of_files[0].split('_')[0], list_of_files[0].split('_')[1], list_of_files[0].split('_')[2] )
 
     burnin = '"[0:v]drawtext=\'fontcolor=white:font=sans-serif:fontsize=10:x=5:y=5:text=  luma-film - 2020 - inside job %s - %s - %s:box=1:boxborderw=5:boxcolor=black\'[LT]"' % (date, shot_name, user)
 
@@ -25,8 +26,8 @@ if len(sys.argv) == 2:
     cmd += ' -filter_complex %s' % burnin
     cmd += ' -pix_fmt yuv420p -c:v libx264 -crf 25 -map "[LT]"'
     cmd += ' %s.mp4' % search_pattern
-    print cmd
+    # print cmd
 
-    # os.system( cmd )
+    os.system( cmd )
 else:
     print 'Boo hoo! Please type lab mv [name of any file in sequence]'
