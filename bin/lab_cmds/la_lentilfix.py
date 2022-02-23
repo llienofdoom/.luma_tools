5f8489a1c0cb9b9505021c7e3c0315ae5c6f9a33
shotalot <shotalot@gmail.com>
2022-02-23 11:04:12 +0200

    iupdated AFTER saving the file...

--- bin/lab_cmds/la_lentilfix.py
+++ bin/lab_cmds/la_lentilfix.py
@@ -34,16 +34,19 @@ list_of_frames = sorted(list_of_frames)
 #COPY
 for cur in list_of_frames:
     # for file in cur:
-    print('origdir: ' + origdir)
+    #print('origdir: ' + origdir)
     filename = cur.split('/')[-1]
+    currentframe=filename.split('.')[-2] 
     print('Doing frame: ' + filename)
     cp_path = origdir + '/lentil/' + filename
-    print('cp path:' + cp_path)
-    print('current"' + cur)
-    shutil.move(cur, cp_path)
-    os.system(
-        'oiiotool %s --attrib "arnold/aovs/RGBA/filter" "RGBA" --attrib "arnold/aovs/variance/filter" "variance_filter" -o %s'
-        % (cp_path, cur))
+    crypto_path = origdir + '/cryptomatte/' + filename.replace("main","cryptomatte")
+    N_path = origdir + '/lentil/' + 'N_noice.' + currentframe + '.exr' 
+    Z_path = origdir + '/lentil/' + 'Z_noice.' + currentframe + '.exr'   
+    #print('cp path:' + cp_path)
+    #print('current"' + cur)
+    #shutil.move(cur, cp_path)
+    #NORMAL
+    os.system('oiiotool %s --ch R,G,B,A,variance.R,variance.G,variance.B,denoise_albedo_noice.R,denoise_albedo_noice.G,denoise_albedo_noice.B %s --ch N_noice.R=N.R,N_noice.G=N.G,N_noice.B=N.B,Z.R,Z.G,Z.B --chappend --chnames R,G,B,A,variance.R,variance.G,variance.B,denoise_albedo_noice.R,denoise_albedo_noice.G,denoise_albedo_noice.B,N_noice.R,N_noice.G,N_noice.B,Z.R,Z.G,Z.B, --attrib "arnold/aovs/RGBA/filter" "RGBA" --attrib "arnold/aovs/variance/filter" "variance_filter" -o %s' % (cur,crypto_path,cp_path))
 
 #FIX
 print('done with lentil filter fix')
