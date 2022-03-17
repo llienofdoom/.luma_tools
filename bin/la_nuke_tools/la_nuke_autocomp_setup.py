878c91b6134486d7558f4d1fde0ef3a0e5d83933
llienofdoom <neill.vermaak@gmail.com>
2022-03-17 12:13:39 +0200

    updated comp thingies

--- bin/la_nuke_tools/la_nuke_autocomp_setup.py
+++ bin/la_nuke_tools/la_nuke_autocomp_setup.py
@@ -100,7 +100,9 @@ if len(sys.argv) is 2:
 
     # Disconnect camera linking
     try:
-        camera_node = nuke.toNode("AUTO_Read_renderCamera")
+        camera_node   = nuke.toNode("AUTO_Read_renderCamera")
+        reload_button = camera_node.knob('reload')
+        reload_button.execute()
         camera_node['read_from_file'].setValue(0)
         camera_node['near'].clearAnimated()
         camera_node['near'].setValue(0.001)
